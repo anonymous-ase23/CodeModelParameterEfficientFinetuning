e683b573f908940f7257968deceac6b8831e3942
anonymous-ase23 <131661403+anonymous-ase23@users.noreply.github.com>
2024-01-02 16:10:29 +0800

    Update utils.py

--- defect/utils.py
+++ defect/utils.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import logging
 import os
@@ -5,24 +6,17 @@ from datasets import load_dataset, load_metric, Dataset
 import torch
 import numpy as np
 import random
-import glob
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 
-def set_seed(seed=42):
-    random.seed(seed)
-    os.environ['PYHTONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
 
 def get_last_checkpoint(dir):
     paths = list(glob.glob(os.path.join(dir, "checkpoint-*")))
     if len(paths) == 0:
-        return None
+        return dir
     last_path_value = paths[0].split("/")[-1].split("-")[1]
     last_path = paths[0]
     for path in paths:
@@ -32,33 +26,41 @@ def get_last_checkpoint(dir):
     return last_path
 
 
-class CodeDataset:
-    def __init__(self, data_dir):
-        self.url_to_code = {}
-        with open(data_dir + '/data.jsonl') as f:
-            for line in f:
-                line = line.strip()
-                js = json.loads(line)
-                self.url_to_code[js['idx']] = js['func']
+def set_seed(seed=42):
+    random.seed(seed)
+    os.environ['PYHTONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
 
+
+import json
+
+
+def read_dataset(filename):
+    source_codes = []
+    source_labels = []
+    with open(filename) as f:
+        for line in f:
+            js = json.loads(line.strip())
+            code = ' '.join(js['func'].split())
+            label = int(js['target'])
+            source_codes.append(code)
+            source_labels.append(label)
+    return source_codes, source_labels
+
+
+class CodeDataset:
     def load(self, file_path, data_num=-1):
-        dataset_dict = {'code1':[], 'code2':[], 'label':[]}
+        dataset_dict = {'code': [], 'label': []}
         count = 0
         with open(file_path) as f:
             for line in f:
-                line = line.strip()
-                url1, url2, label = line.split('\t')
-                if url1 not in self.url_to_code or url2 not in self.url_to_code:
-                    continue
-
+                js = json.loads(line.strip())
+                dataset_dict['code'].append(' '.join(js['func'].split()))
+                dataset_dict['label'].append(int(js['target']))
                 count += 1
-                dataset_dict['code1'].append(self.url_to_code[url1])
-                dataset_dict['code2'].append(self.url_to_code[url2])
-                dataset_dict['label'].append(int(label))
                 if count == data_num:
                     break
         return Dataset.from_dict(dataset_dict)
-
-
-
-
